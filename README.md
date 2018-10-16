f76fda46f54c4e5094f4c2b6ce1168079fe255c1
tinawpxp <40365605+tinawpxp@users.noreply.github.com>
2018-10-15 20:47:37 -0700

    Update and rename README.md to assignment-1.js

--- README.md
+++
@@ -1,2 +0,0 @@
-# bs-javascriptassignment-
-# bs-javascriptassignment-
---
+++ assignment-1.js
@@ -0,0 +1,20 @@
+
+var numbers = [];
+
+for (var i=0; i<10; i++){
+	numbers.push([i]);
+}
+
+console.log([0]);
+
+console.log([10]);
+
+var car = {};
+
+car.colour = ("black");
+
+
+
+  
+  
+  
